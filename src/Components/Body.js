c7fa014f82f9566b8313e0104fc362897bf5e268
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-07-10 20:28:01 +0500

    some changes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -58,10 +58,7 @@ export default function ReviewCard() {
         title="Paella dish"
       /></Link>
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
+        
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
